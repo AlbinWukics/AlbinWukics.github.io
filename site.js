b8ac6552f3c92b8b78fcf3fc699e94fb56e702b7
AlbinWukics <112380147+AlbinWukics@users.noreply.github.com>
2023-02-12 21:21:37 +0100

    shop

---
+++ site.js
@@ -0,0 +1,23 @@
+// FUNKAR INTE HAHAHA
+// const productOne = document.querySelector("#products");
+
+// displayProducts();
+
+// function displayProducts() {
+//   const column = document.createElement("div");
+//   const image = document.createElement("img");
+//   const headerCost = document.createElement("h3");
+//   const addToCartBtn = document.createElement("button");
+
+//   column.classList.add("col", "text-center");
+//   image.src = "bilder/brödprodukt1.webp";
+//   image.alt = "bröd produkt 1";
+//   image.classList.add("image-fluid");
+//   headerCost.classList.add("mt-2");
+//   headerCost.innerText = "40 kr";
+//   addToCartBtn.type = "button";
+//   addToCartBtn.classList.add("btn", "btn-outline-info", "btn-lg", "mt-2");
+//   addToCartBtn.innerText = "Lägg till i varukorg";
+
+//   column.append(image, headerCost, addToCartBtn);
+// }
